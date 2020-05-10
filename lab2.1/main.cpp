a779621b0effe2973a92faa20c729a190aff6ce8
filonovsv <36759848+filonovsv@users.noreply.github.com>
2020-05-11 05:41:18 +0700

    Update main.cpp

--- lab2.1/main.cpp
+++ lab2.1/main.cpp
@@ -1,3 +1,39 @@
-int main(){
+#include <iostream>
+#include <omp.h>
+#include "SquareMatrix.h"
+#include "Solver.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  SquareMatrix A;
+  A = { {1.0, 2.0}, 
+        {3.0 ,1.0} }; 
+  vector<double> b;
+  b = { 1.0, 1.0 };
+  
+  omp_set_num_threads(atoi(argv[1]));
+  vector<double> x;
+  double start = omp_get_wtime();
+  try {
+    Solver solver;
+    x = solver.solve(A, b);
+  }
+  catch (exception e) {
+    cout << e.what();
+    return -1;
+  }
+  double finish = omp_get_wtime();
+  
+  for (auto i : x) {
+    cout << i << " ";
+  }
+
+  cout << endl;
+
+  cout << finish - start;
+
+  getchar();
   return 0;
 }
