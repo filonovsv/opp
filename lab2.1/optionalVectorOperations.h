ad3159a7627fb6f7f89c0bb8a1f7b2720b0d9743
filonovsv <36759848+filonovsv@users.noreply.github.com>
2020-05-13 12:48:39 +0700

    Update and rename optionalVectorOperations.h to LinearAlgebraSubprograms.h

--- lab2.1/optionalVectorOperations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-using namespace std;
-
-double operator*(vector<double> lhs, vector<double> rhs);
-vector<double> operator*(double lhs, vector<double> rhs);
-vector<double> operator-(vector<double> lhs, vector<double> rhs);
-vector<double> operator+(vector<double> lhs, vector<double> rhs);
-double norma(vector<double> a);
