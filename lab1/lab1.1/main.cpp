0177cab4ed86f2b07e9f9e36b5d310ce50a11a65
filonovsv <36759848+filonovsv@users.noreply.github.com>
2020-05-31 01:36:07 +0700

    Update main.cpp

--- lab1/lab1.1/main.cpp
+++ lab1/lab1.1/main.cpp
@@ -28,53 +28,46 @@ int main(int argc, char** argv) {
 
   double* A = new double[size * revcounts[mpi_rank]];
 
-  int current_rank = 0;
-  while (current_rank < mpi_size) {
-    if (mpi_rank == current_rank) {
-      for (size_t i = 0; i < revcounts[mpi_rank]; ++i)
-      {
-        for (size_t j = 0; j < size; ++j) {
-          A[i*size + j] = 1.0;
-          if (displs[mpi_rank] + i  == j ) {
-            A[i*size +j] = 2.0;
-          }
-        }
+  for (size_t i = 0; i < revcounts[mpi_rank]; ++i) {
+    for (size_t j = 0; j < size; ++j) {
+      A[i*size + j] = 1.0;
+      if (displs[mpi_rank] + i == j) {
+        A[i*size + j] = 2.0;
       }
     }
-    ++current_rank;
-    MPI_Barrier(MPI_COMM_WORLD);
   }
 
-  double* b = new double[size];
-  double* x = new double[size];
 
-  for (size_t i = 0; i < size; ++i) {
-    b[i] = (double)size + 1.0;
-    x[i] = 0.0;
-  }
- 
-  double start = MPI_Wtime();
+    double* b = new double[size];
+    double* x = new double[size];
 
-  solve(A, b, size, x, mpi_size, mpi_rank, revcounts, displs);
-
-
-  double finish = MPI_Wtime();
-
-  if (mpi_rank == 0) {
-    std::cout << "result: ";
-    for (size_t i = 0; i < size; ++i)
-    {
-      std::cout << x[i] << " ";
+    for (size_t i = 0; i < size; ++i) {
+      b[i] = (double)size + 1.0;
+      x[i] = 0.0;
     }
-    std::cout << std::endl;
-    std::cout << "for: " << finish - start;
+
+    double start = MPI_Wtime();
+
+    solve(A, b, size, x, mpi_size, mpi_rank, revcounts, displs);
+
+
+    double finish = MPI_Wtime();
+
+    if (mpi_rank == 0) {
+      std::cout << "result: ";
+      for (size_t i = 0; i < size; ++i)
+      {
+        std::cout << x[i] << " ";
+      }
+      std::cout << std::endl;
+      std::cout << "for: " << finish - start;
+    }
+
+    thread_manager_finalize(revcounts, displs);
+    MPI_Finalize();
+    delete[] A;
+    delete[] b;
+    delete[] x;
+
+    return 0;
   }
-
-  thread_manager_finalize(revcounts, displs);
-  MPI_Finalize();
-  delete[] A;
-  delete[] b;
-  delete[] x;
-
-  return 0;
-}
